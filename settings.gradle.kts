99484559e89f41f6a211cc11e8168a60ddbeda7f
сергей ишкин <sergeja.ishkin@gmail.com>
2026-08-16 19:26:26 +0300

    fix1.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
