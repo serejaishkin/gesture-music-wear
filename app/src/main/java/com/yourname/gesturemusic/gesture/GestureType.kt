fbfc32a6b8f8a2d7f729890e99c8c088b0cbc430
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-08-18 06:59:26 +0300

    Add trainable activation gesture

--- app/src/main/java/com/yourname/gesturemusic/gesture/GestureType.kt
+++ app/src/main/java/com/yourname/gesturemusic/gesture/GestureType.kt
@@ -1,6 +1,8 @@
 package com.yourname.gesturemusic.gesture
 
 enum class GestureType {
+    /** Deliberate gesture that arms media control for a short period. */
+    ACTIVATE,
     NEXT_TRACK,
     PREVIOUS_TRACK,
     PLAY_PAUSE
